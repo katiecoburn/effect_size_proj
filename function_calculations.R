56cf107c282356f0e03670d8c3226d01b6d05224
katiecoburn <katie.m.coburn@gmail.com>
2020-12-21 13:46:28 -0600

    fixed to use parameter values in adjustment parts of functions and estimates in other parts

--- function_calculations.R
+++ function_calculations.R
@@ -1,14 +1,14 @@
 trad_es_calc <- function(delta, msw, mswb, mswab, m, n, sigma_error_pop, sigma_block_pop, 
                          sigma_int_pop, sigma_total_pop, ...){
   
-  sigma_error <- msw
-  sigma_block <- (mswb - msw)/(2*n)
-  sigma_int <- (mswab - msw)/n
-  sigma_total <- sigma_error + sigma_block + sigma_int
+  sigma_error_est <- msw
+  sigma_block_est <- (mswb - msw)/(2*n)
+  sigma_int_est <- (mswab - msw)/n
+  sigma_total_est <- sigma_error_est + sigma_block_est + sigma_int_est
   
   N <- 2 * m * n
   
-  d2 <- delta/sqrt(sigma_total_pop)
+  d2 <- delta/sqrt(sigma_total_est)
   
   v2 <- (2/(m*n)) + (d2^2)/(2 * (N - 2))
   
@@ -20,18 +20,18 @@ par_est_calc <- function(delta, msw, mswb, mswab, m, n, sigma_error_pop, sigma_b
                          sigma_int_pop, sigma_total_pop, ...){
   # this is the version in excel spreadsheet karina sent on 11/18/20
   
-  sigma_error <- msw
-  sigma_block <- (mswb - msw)/(2*n)
-  sigma_int <- (mswab - msw)/n
-  sigma_total <- sigma_error + sigma_block + sigma_int
+  sigma_error_est <- msw
+  sigma_block_est <- (mswb - msw)/(2*n)
+  sigma_int_est <- (mswab - msw)/n
+  sigma_total_est <- sigma_error_est + sigma_block_est + sigma_int_est
   
   N <- 2 * m * n
   
-  d1 <- sqrt(((N - 2 * m) * sigma_error + (m - 1) * (sigma_error + 2 * n * sigma_block) + (m - 1) * (sigma_error + n * sigma_int))/((N - 2) * sigma_total)) * delta/sqrt(sigma_total_pop)
+  d1 <- sqrt(((N - 2 * m) * sigma_error_pop + (m - 1) * (sigma_error_pop + 2 * n * sigma_block_pop) + (m - 1) * (sigma_error_pop + n * sigma_int_pop))/((N - 2) * sigma_total_pop)) * delta/sqrt(sigma_total_est)
   
-  v1_term_1 <- (2/(m * n)) * ((sigma_error_pop + n * sigma_int_pop)/sigma_total_pop)
-  v1_term_2_num <- ((N - 2 * m) * sigma_error^2 + (m - 1) * (sigma_error + 2 * n * sigma_block)^2 + (m - 1) * (sigma_error + n * sigma_int)^2) * d1^2
-  v1_term_2_denom <- 2 * ((N - 2 * m) * sigma_error + (m - 1) * (sigma_error + 2 * n * sigma_block) + (m - 1) * (sigma_error + n * sigma_int))^2
+  v1_term_1 <- (2/(m * n)) * ((sigma_error_est + n * sigma_int_est)/sigma_total_est)
+  v1_term_2_num <- ((N - 2 * m) * sigma_error_pop^2 + (m - 1) * (sigma_error_pop + 2 * n * sigma_block_pop)^2 + (m - 1) * (sigma_error_pop + n * sigma_int_pop)^2) * d1^2
+  v1_term_2_denom <- 2 * ((N - 2 * m) * sigma_error_pop + (m - 1) * (sigma_error_pop + 2 * n * sigma_block_pop) + (m - 1) * (sigma_error_pop + n * sigma_int_pop))^2
   
   v1 <- v1_term_1 + v1_term_2_num/v1_term_2_denom
   
